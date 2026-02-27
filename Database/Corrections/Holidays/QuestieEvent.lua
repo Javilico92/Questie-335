e42f89a49e24e0092d82d3d8ff990369094211c5
Javilico <javilico92@gmail.com>
2026-02-27 10:53:04 +0100

    - Updated Questie to v10.0.1 as there are not code changes (im trying to update to 10.11.0 asap)

--- Database/Corrections/Holidays/QuestieEvent.lua
+++ Database/Corrections/Holidays/QuestieEvent.lua
@@ -339,7 +339,8 @@ QuestieEvent.eventDates = {
         endDate = "28/11"
     },
     ["Hallow's End"] = {startDate = "18/10", endDate = "31/10"},
-    ["Winter Veil"] = {startDate = "15/12", endDate = "1/1"}
+    ["Winter Veil"] = {startDate = "15/12", endDate = "1/1"},
+    ["Day of the Dead"] = {startDate = "1/11", endDate = "2/11"},
 }
 
 -- ["EventName"] = false -> event doesn't exists in expansion
